c49c0d0ef68441e191c8129b72cf8e4860a37118
MARTINPC\marti <martin_sokolov@yahoo.com>
2023-08-06 12:57:47 +0300

    Changed DB to postgres and updated rquirements.txt

--- DIY_comunity/DIY_comunity/settings.py
+++ DIY_comunity/DIY_comunity/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'DIY_comunity.accounts',
+    'DIY_comunity.common',
+    'DIY_comunity.categories',
+    'DIY_comunity.projects',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +80,13 @@ WSGI_APPLICATION = 'DIY_comunity.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "my-database",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
