c77c92ef0cc5c891168568748e618ae63638d1e5
MARTINPC\marti <martin_sokolov@yahoo.com>
2023-08-08 16:50:05 +0300

    Implemented categories

--- DIY_comunity/DIY_comunity/categories/views.py
+++ DIY_comunity/DIY_comunity/categories/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views import generic as views
 
+from DIY_comunity.categories.models import CategoryModel
+
 
 # Create your views here.
 
 class CategoriesListView(views.ListView):
-    pass
+    model = CategoryModel
+    template_name = 'categories/categories-list-page.html'  # Replace with your template name
+    context_object_name = 'categories'
 
 
-class CategoryProjectsListView(views.ListView):
-    pass
+class CategoryProjectsListView(views.DetailView):
+    model = CategoryModel
+    template_name = 'categories/category-projects-list-page.html'  # Replace with your template name
+    context_object_name = 'category'
